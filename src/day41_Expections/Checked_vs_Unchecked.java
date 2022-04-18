b1356230ccbc6916a6898daf7b803a67bbc2cfc3
tugcee35 <100022500+tugcee35@users.noreply.github.com>
2022-04-18 19:17:58 +0300

    YAAYY

--- src/day41_Expections/Checked_vs_Unchecked.java
+++ src/day41_Expections/Checked_vs_Unchecked.java
@@ -1,5 +1,7 @@
 package day41_Expections;
 
+import day39_Recap.cydeoTask.Student;
+
 public class Checked_vs_Unchecked {
     public static void main(String[] args) {
 
@@ -12,7 +14,20 @@ public class Checked_vs_Unchecked {
 
         char[] characters = {'A', 'B', 'C'};
 
-        System.out.println(characters[99]);
+       // System.out.println(characters[99]);
+
+        //Student student = null;
+        //System.out.println(student.getName());
+
+        final String str = "Wooden Spoon";
+       // str = null;
+        //System.out.println(str.toUpperCase());
+        String str2="";
+        System.out.println(str2.isEmpty());
+
+//Checked Exception
+        System.out.println("Hello");
+        System.out.println("Cydeo");
 
 
     }
---
+++ src/day41_Expections/TryCatchBlocks.java
@@ -0,0 +1,7 @@
+package day41_Expections;
+
+public class TryCatchBlocks {
+    public static void main(String[] args) {
+
+    }
+}
