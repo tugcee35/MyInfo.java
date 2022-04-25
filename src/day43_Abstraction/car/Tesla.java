446d1a3f1ca6f0454d621c3c72fdfc03eba8a280
tugcee35 <100022500+tugcee35@users.noreply.github.com>
2022-04-25 15:51:06 +0300

    YAAYY

--- src/day43_Abstraction/car/Tesla.java
+++ src/day43_Abstraction/car/Tesla.java
@@ -2,8 +2,8 @@ package day43_Abstraction.car;
 
 public final class Tesla extends Car{
 
-    public Tesla(String brand, String model, String color, int year, double price) {
-        super(brand, model, color, year, price);
+    public Tesla(String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
     }
 
     @Override
